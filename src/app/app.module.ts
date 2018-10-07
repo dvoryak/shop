11238afa7da5164fdfd7b0589486c7b94d972ab5
Pavlo Dvoriak <pavlo_dvoriak@epam.com>
2018-10-07 12:30:37 +0300

    Add services and register them im Core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {ProductModule} from './products/components/product.module';
 import {CartModule} from './cart/components/cart.module';
+import { ContactUsComponent } from './contact-us/components/contact-us/contact-us.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ContactUsComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/core/index.ts
@@ -0,0 +1,4 @@
+export * from './services/config-option.service';
+export * from './services/constants.service';
+export * from './services/generator.service';
+export * from './services/local-storage.service';
