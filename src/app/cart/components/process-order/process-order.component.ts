637e4e86e560189d6b0a478900d7d3bdac3bf50e
Pavlo Dvoriak <pavlo_dvoriak@epam.com>
2018-10-28 15:22:32 +0200

    Prepare validation logic & add validation for firstName

--- src/app/cart/components/process-order/process-order.component.ts
+++ src/app/cart/components/process-order/process-order.component.ts
@@ -1,24 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CartService} from '../../services/cart.service';
 import {CartProduct} from '../../models/cart-product.model';
-import {FormGroup} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
-  selector: 'app-process-order',
-  templateUrl: './process-order.component.html',
-  styleUrls: ['./process-order.component.css']
+    selector: 'app-process-order',
+    templateUrl: './process-order.component.html',
+    styleUrls: ['./process-order.component.css']
 })
-export class ProcessOrderComponent implements OnInit {
+export class ProcessOrderComponent implements OnInit, OnDestroy {
 
-  products: CartProduct[];
+    products: CartProduct[];
 
-  formGroup: FormGroup;
+    formGroup: FormGroup;
 
-  constructor(cartService: CartService) {
-    cartService.findAll().subscribe(data => this.products = data);
-  }
+    private validationMessages = {
+        firstName: {
+            required: 'Please enter your email address.',
+            maxLength: 'Make sure that your name less then 50 characters'
+        },
 
-  ngOnInit() {
-  }
+        lastName: {
+            required: 'Please enter your last name',
+            maxLength: 'Make sure that your name less then 50 characters'
+        },
+    };
+
+    private sub: Subscription = new Subscription();
+    private validationMessage: string;
+
+    constructor(private cartService: CartService,
+                private fb: FormBuilder) {
+        cartService.findAll().subscribe(data => this.products = data);
+    }
+
+    ngOnInit() {
+        this.buildForm();
+        this.watchValueChanges();
+    }
+
+    private buildForm() {
+        this.formGroup = this.fb.group({
+            firstName: ['',
+               [Validators.required, Validators.maxLength(50)]],
+            lastName: ['',
+                [Validators.required, Validators.maxLength(50)]],
+            city: ['',
+                [Validators.required, Validators.maxLength(50)]],
+            address: ['',
+                [Validators.required, Validators.maxLength(20)]],
+            email: ['',
+                [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+'), Validators.email]
+            ],
+            phone: ['',
+                [Validators.required, Validators.maxLength(12)]],
+            comment: ''
+        });
+    }
+
+    private watchValueChanges() {
+        const firstName = this.formGroup.get('firstName');
+
+        const subFirstName = firstName.valueChanges.subscribe(data => {
+                this.setValidationMessage(firstName, 'firstName');
+            }
+        );
+
+        this.sub.add(subFirstName);
+    }
+
+    private setValidationMessage(control: AbstractControl, controlName: string): void {
+        this.validationMessage = '';
+
+        if ((control.touched || control.dirty) && control.errors) {
+            console.log('e', control);
+            this.validationMessage = Object.keys(control.errors)
+                .map(key => this.validationMessages[controlName][key])
+                .join(' ');
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
+    }
 
 }
